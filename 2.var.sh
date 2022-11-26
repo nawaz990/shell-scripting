a5267049e8b2f46ebdd7accf6beffd0bd32fac10
nawaz990 <yasirnawaz990@gmail.com>
2022-11-26 17:43:18 +0530

    var script

---
+++ 2.var.sh
@@ -0,0 +1,10 @@
+##Varible - variables are nothing but, if we assign aname to (parent dic)a set of data is called variable .
+
+#Declare a variable
+# VARNAME=DATA (No spaces)
+a=10
+
+# Access variable - $VARNAME - Meaning $ symbol prefixing it
+echo A - $a
+
+
