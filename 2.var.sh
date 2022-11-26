66d8320d4457c2fa14637c0b64feab3925da545a
nawaz990 <yasirnawaz990@gmail.com>
2022-11-26 18:02:51 +0530

    var script

--- 2.var.sh
+++ 2.var.sh
@@ -7,4 +7,11 @@ a=10
 # Access variable - $VARNAME - Meaning $ symbol prefixing it
 echo A - $a
 
+# a=10 is hardcoded manually we need the data dynamically. this
+## Dynamically you can get in two ways.
+# 1. user input.
+# 2. Script find out with other commands
+
+# command Substitution
+# VARNAME=$(command)
 
