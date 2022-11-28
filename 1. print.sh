c02f1fdb5f231f28987e61f190223dba03b75ded
nawaz990 <yasirnawaz990@gmail.com>
2022-11-29 02:05:44 +0530

    Mysql script

--- 1. print.sh
+++ 1. print.sh
@@ -1,2 +1,10 @@
 echo "Hello world"
 
+# Colour Syntax
+# echo -e "\e[COLmHELLOWORLD\e[0m"
+# -e -enable \e
+# \e -enable COL
+# COL - COL CODE , 31-36 (31 - Red, 32 - Green, 33 - Yellow, 34 - Blue, 35 - Magenta, 36 - Cyan)
+# 0m - Disable the enable colour
+
+echo -e "\e[31mHELLOWORLD in Red Colour\e[0m"
