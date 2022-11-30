0c4ea054e1b1e89de6b4300d84b3383aea393de3
nawaz990 <yasirnawaz990@gmail.com>
2022-12-01 01:46:39 +0530

    input script

--- 04.input.sh
+++ 04.input.sh
@@ -1,3 +1,4 @@
-read -p 'Enter MySQL Root Password: ' pass
+read -p 'Enter MySQL Root Username: ' user
+read -s -p 'Enter MySQL Root Password: ' pass
 
-echo Password from read = $pass
+echo "Username & Password from read = $user & $pass"
